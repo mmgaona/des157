c5d21c2fb0fc52a2a16dc44aca24cf48dec9fa11
mmgaona <35280694+mmgaona@users.noreply.github.com>
2018-01-18 13:37:43 -0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,21 +1,43 @@
+var x=50;
+var y=50;
+
 function setup() {
-var myCanvas = createCanvas (500,500);
+var myCanvas = createCanvas (800,250);
 myCanvas.parent ('mySketch');
-background(255);
 }
 
 function draw (){
-  // if (mouseIsPressed==true){
-  //   fill(255);
-  //   rectMode(CENTER);
-  //   rect(mouseX, mouseY, 80, 80);
-  // }else{
+
     noCursor();
+    // noFill();
+    // stroke(mouseX,100,mouseY);
+    // rectMode(CENTER);
+    // rect(mouseX,mouseY,x,y);
     stroke(mouseX,100,mouseY);
-    ellipse(mouseX, mouseY, 50, 50);
+    noFill();
+    ellipse(mouseX, mouseY, x, y);
+    x=x+4;
+    y=y+4;
+
+    if (x>500) {
+      x=0;
+      clear();
+    }
+
+    if (y>500) {
+      y=0;
+    }
 
 }
 
 function mousePressed() {
-  clear();
+  x=0;
+  y=0;
+  background(random(0,255),random(0,255),random(0,255),90);
+}
+
+function mouseDragged() {
+  fill(mouseX,100,mouseY);
+  ellipse(mouseX,mouseY,x,y);
+
 }
