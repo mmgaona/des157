d509200952953443a1a4eb820e0effaa48b712fb
Martin <mmgaona@ucdavis.edu>
2018-01-30 13:40:37 -0800

    final

--- studio-01/script.js
+++ studio-01/script.js
@@ -11,12 +11,12 @@ var smoothie5 = document.form.smoothie5.value;
 var smoothie6 = document.form.smoothie6.value;
 var smoothie7 = document.form.smoothie7.value;
 var smoothie8 = document.form.smoothie8.value;
-var smoothie9 = document.form.smoothie9.value;
 
 var myMsg = document.getElementById("myMsg");
 
-myMsg.innerHTML = smoothie1 + " " smoothie2 + " " + smoothie3 + " " + smoothie4 + " " + 
-smoothie5 + " " + smoothie6 + " " + smoothie7 + " " + smoothie8 + " " + smoothie9;
+myMsg.innerHTML = ["First, get out your NutriBullet. If you don't have one, get one. Next, do not even think about using " + smoothie1 + ", ever. " + " You can use " + smoothie2 + ", sometimes. Next, grab as many "
++ smoothie3 + " as you can. If you can find " + smoothie4 + " ones, use those. Next, go to your local " + smoothie5 + " market, and buy all the " + smoothie6 + " they have. Now, " + smoothie7 + " throw all the ingredients in your NutriBullet and then fill with "
++ smoothie8 + ". Finally, smash that blend down and enjoy! "];
 
 myMsg.className = "show";
 
