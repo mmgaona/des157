f338fefe52359a89aff563b80ca7894080e28ae7
Martin <mmgaona@ucdavis.edu>
2018-02-08 02:54:40 -0800

    studio2

--- studio-01/script.js
+++ studio-01/script.js
@@ -1,30 +1,37 @@
 document.form.onsubmit = processForm; //function for submit
 document.form.onreset = clearForm; //function for reset
 
+
+//declaring process/submit function//
 function processForm() {
 
-var smoothie1 = document.form.smoothie1.value;
-var smoothie2 = document.form.smoothie2.value;
-var smoothie3 = document.form.smoothie3.value;
-var smoothie4 = document.form.smoothie4.value;
-var smoothie5 = document.form.smoothie5.value;
-var smoothie6 = document.form.smoothie6.value;
-var smoothie7 = document.form.smoothie7.value;
-var smoothie8 = document.form.smoothie8.value;
+//input variables//
+var smoothie1 = document.form.smoothie1.value; //favorite fruit
+var smoothie2 = document.form.smoothie2.value; //favorite vegetable
+var smoothie3 = document.form.smoothie3.value; //least favorite fruit
+var smoothie4 = document.form.smoothie4.value; //favorite color
+var smoothie5 = document.form.smoothie5.value; //favorite cuisine
+var smoothie6 = document.form.smoothie6.value; //noun
+var smoothie7 = document.form.smoothie7.value; //adverb
+var smoothie8 = document.form.smoothie8.value; //favorite alcoholic beverage
 
 var myMsg = document.getElementById("myMsg");
 
+//output message//
 myMsg.innerHTML = ["First, get out your NutriBullet. If you don't have one, get one. Next, do not even think about using " + smoothie1 + ", ever. " + " You can use " + smoothie2 + ", sometimes. Next, grab as many "
 + smoothie3 + " as you can. If you can find " + smoothie4 + " ones, use those. Next, go to your local " + smoothie5 + " market, and buy all the " + smoothie6 + " they have. Now, " + smoothie7 + " throw all the ingredients in your NutriBullet and then fill with "
 + smoothie8 + ". Finally, smash that blend down and enjoy! "];
 
-myMsg.className = "show";
+//show message//
+myMsg.className = "show"; //class to show message
 
+//stops reloading//
 return false;
 
 }
 
+//declaring clear/reset function//
 function clearForm() {
     var myMsg = document.getElementById("myMsg");
-		myMsg.className = "hide";
+		myMsg.className = "hide"; //class to hide message
 }
---
+++ studio-02/script.js
@@ -0,0 +1,116 @@
+var backpackButton = document.getElementById('backpackButton');
+var backpack = document.getElementById('backpack');
+var notebook = document.getElementById('notebook');
+var backpackButton = document.getElementById('backpackButton');
+var backpack = document.getElementById('backpack');
+var title = document.getElementById('title');
+var close = document.getElementById('close');
+var footer = document.getElementById('footer');
+var closeButton = document.getElementById('closeButton');
+var img = document.getElementsByTagName('img');
+
+iphoneButton.addEventListener('mouseover', function() {
+  iphoneButton.style.opacity="0.4";
+  if (iphoneButton.style.width > "100px") {
+    iphoneButton.style.opacity="1";
+  }
+});
+
+iphoneButton.addEventListener('mouseout', function() {
+  iphoneButton.style.opacity="1";
+});
+
+notebookButton.addEventListener('mouseover', function() {
+  notebookButton.style.opacity="0.4";
+  if (notebookButton.style.width > "250px") {
+    notebookButton.style.opacity="1";
+  }
+});
+
+notebookButton.addEventListener('mouseout', function() {
+  notebookButton.style.opacity="1";
+});
+
+backpackButton.addEventListener('mouseover', function() {
+  backpackButton.style.opacity="0.4";
+  if (backpackButton.style.width > "200px") {
+    backpackButton.style.opacity="1";
+  }
+});
+
+backpackButton.addEventListener('mouseout', function() {
+  backpackButton.style.opacity="1";
+});
+
+
+backpackButton.addEventListener('click', function() {
+  backpack.style.display="inline";
+  iphone.style.display="none"
+  notebook.style.display="none"
+  backpack.style.marginTop="-58%";
+  iphoneButton.style.display="none";
+  notebookButton.style.display="none";
+  backpackButton.style.width="450px";
+  backpackButton.style.marginLeft="-34%";
+  close.style.backgroundColor="#f4e52a";
+  title.style.display="none";
+  // close.style.marginTop="-42.3%";
+  close.style.display="inline";
+  footer.style.display="none";
+});
+
+notebookButton.addEventListener('click', function() {
+  notebook.style.display="inline";
+  backpack.style.display="none";
+  iphone.style.display="none";
+  notebook.style.marginTop="-58%";
+  backpackButton.style.display="none";
+  iphoneButton.style.display="none";
+  notebookButton.style.width="500px";
+  notebookButton.style.marginLeft="-40%";
+  notebookButton.style.marginTop="9%";
+  close.style.backgroundColor="#003840";
+  title.style.display="none";
+  // close.style.marginTop="-33.5%";
+  close.style.display="inline";
+  footer.style.display="none";
+});
+
+iphoneButton.addEventListener('click', function() {
+  notebook.style.display="none";
+  backpack.style.display="none";
+  iphone.style.display="inline";
+  iphone.style.marginTop="-58%";
+  backpackButton.style.display="none";
+  notebookButton.style.display="none";
+  iphoneButton.style.width="250px";
+  iphoneButton.style.marginLeft="-25%";
+  iphoneButton.style.paddingRight="10%";
+  close.style.backgroundColor="#e31985";
+  title.style.display="none";
+  // close.style.marginTop="-45.5%";
+  close.style.display="inline";
+  footer.style.display="none";
+});
+
+
+closeButton.addEventListener('mouseover', function() {
+closeButton.style.cursor="pointer";
+});
+
+closeButton.addEventListener('click', function() {
+  close.style.display="none";
+  title.style.display="grid";
+  backpackButton.style.display="inline";
+  notebookButton.style.display="inline";
+  iphoneButton.style.display="inline";
+  iphoneButton.style.paddingRight="0%";
+  iphoneButton.style.marginLeft="0.1%";
+  notebookButton.style.marginLeft="0.1%";
+  backpackButton.style.marginLeft="0.1%";
+  backpackButton.style.paddingLeft="3%";
+  backpackButton.style.width="200px";
+  notebookButton.style.width="250px";
+  iphoneButton.style.width="100px";
+  footer.style.display="block";
+});
