aec5d1cda8f66604b4b4875f8da597353dc4c18a
Martin <mmgaona@ucdavis.edu>
2018-02-22 14:38:57 -0800

    studio 03

--- studio-03/js/script.js
+++ studio-03/js/script.js
@@ -1,25 +1,36 @@
-
-console.clear();
+// Array for rows of synths
 
 var synths = [new Tone.Synth(), new Tone.Synth(), new Tone.Synth()];
+
+// Declaring an oscillator type for each synth for a frequency wave
 
 synths[0].oscillator.type = 'sine';
 synths[1].oscillator.type = 'sine';
 synths[2].oscillator.type = 'sine';
 
+// Adjusting volume level(gain) to speakers(toMaster)
+
 var gain = new Tone.Gain(2);
 gain.toMaster();
+
+// Connecting synths output to adjusted volume level
 
 synths.forEach(function (synth) {
   return synth.connect(gain);
 });
 
+// Assigning notes to array of rows (synths) by selecting nested divs
+
 var $rows = document.querySelectorAll('div > div'),
     notes = ['C', 'E', 'G'];
 var index = 0;
 
+// Function sound start and for repeat on 8th note
+
 Tone.Transport.scheduleRepeat(repeat, '8n');
 Tone.Transport.start();
+
+// Funtion to loop notes with a for loop and if statement with input checked
 
 function repeat(time) {
   var octave = index % 8;
